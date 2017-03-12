519a75733a8e408f2b480665d91145604934f60d
r2h2 <rainer@hoerbe.at>
2017-03-12 19:02:54 +0100

    make generic

--- install/scripts/pkcs11_token_init.sh
+++ install/scripts/pkcs11_token_init.sh
@@ -4,13 +4,13 @@
 [ -z "$TOKENPW" ] && TOKENPW='secret1'
 [ -z "$SecurityOfficerPIN" ] && SecurityOfficerPIN='secret2'
 echo 'Initializing Token'
-pkcs11-tool --module /usr/lib64/libeToken.so --init-token --label test --pin $TOKENPW --so-pin $SecurityOfficerPIN || exit -1
+pkcs11-tool --module $PKCS11_CARD_DRIVER --init-token --label test --pin $TOKENPW --so-pin $SecurityOfficerPIN || exit -1
 
 echo 'Initializing User PIN'
-pkcs11-tool --module /usr/lib64/libeToken.so --login --init-pin --pin $TOKENPW --so-pin $SecurityOfficerPIN
+pkcs11-tool --module $PKCS11_CARD_DRIVER --login --init-pin --pin $TOKENPW --so-pin $SecurityOfficerPIN
 
 echo 'Generating RSA key'
-pkcs11-tool --module /usr/lib64/libeToken.so --login --keypairgen --key-type rsa:2048 -d 1 --label test --pin $TOKENPW || exit -1
+pkcs11-tool --module $PKCS11_CARD_DRIVER --login --keypairgen --key-type rsa:2048 -d 1 --label test --pin $TOKENPW || exit -1
 
-echo 'Checking objects on eToken'
-pkcs11-tool --module /usr/lib64/libeToken.so --login -O --pin $TOKENPW || exit -1
+echo 'Checking objects on card'
+pkcs11-tool --module $PKCS11_CARD_DRIVER --login -O --pin $TOKENPW || exit -1
