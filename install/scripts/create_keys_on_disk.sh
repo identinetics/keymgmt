3bbf1b8616665bb339f724c2a94b96efc79faae6
r2h2 <rainer@hoerbe.at>
2017-03-13 19:27:39 +0100

    add key-to-token upload; improve key generation

--- install/scripts/create_keys_on_disk.sh
+++ install/scripts/create_keys_on_disk.sh
@@ -10,43 +10,11 @@ main() {
 }
 
 
-yyget_commandline_opts() {
-    echo " $# $@"
-    USAGE=$(printf "%s\n" \
-        "usage: $0 [-h] [-p] <cn> " \
-        "  -h  print this help text" \
-        "  -p  print command") \
-        "  <cn>  common name part of x509 subject"
-
-    while getopts ":p" opt; do
-      case $opt in
-        p) PRINT="True";;
-        *) echo $USAGE; exit 0;;
-      esac
-    done
-    shift $((OPTIND-1))
-    if [ -z $1 ]; then
-        echo 'missing argument "common name"'
-        echo $USAGE
-        exit 1
-    fi
-    CommonName=$1
-}
-
-
-
 get_commandline_opts() {
     keysize=2048
-    x509subject='C=AT/ST=Wien/L=Wien/O=Testfirmat/OU=IT/CN=localhost'
+    x509subject='C=AT/ST=Wien/L=Wien/O=Testfirma/OU=IT/CN=localhost'
     keyname='signer'
-    USAGE=$(printf "%s\n" \
-        "usage: $0 [-k <NNNN>] [-n <key name>] " \
-        "  -h  print this help text" \
-        "  -k  keysize (default: $keysize)" \
-        "  -n  file name of key and certificate (default: '$keyname')" \
-        "  -p  print command" \
-        "  -s  x509 subject (default: 'x509subject')")
-    while getopts ":k:n:ps:" opt; do
+    while getopts ":k:n:s:v" opt; do
       case $opt in
         k) keysize=$OPTARG
            re='^[0-9]{3,5}$'
@@ -54,13 +22,25 @@ get_commandline_opts() {
               echo "error: -k argument is not a number in the range from 1024 .. 16384" >&2; exit 1
            fi;;
         n) keyname=$OPTARG;;
-        p) PRINT="True";;
+        v) verbose="True";;
         s) x509subject=$OPTARG;;
         :) echo "Option -$OPTARG requires an argument"; exit 1;;
-        *) echo $usage; exit 0;;
+        *) usage; exit 0;;
       esac
     done
     #shift $((OPTIND-1))
+}
+
+
+usage() {
+    cat << EOF
+        usage: $0 [-k <NNNN>] [-n <key name>] [-v] [-s <X509 subject>]
+          -h  print this help text
+          -k  keysize (default: $keysize)
+          -n  file name of key and certificate (default: $keyname)
+          -v  print command
+          -s  x509 subject DN (default: $x509subject)
+EOF
 }
 
 
@@ -94,7 +74,7 @@ create_keypair_and_certificate() {
     "
     cmd2="openssl rsa -in /ramdisk/${keyname}_key_pkcs8.pem -out /ramdisk/${keyname}_key_pkcs1.pem"
 
-    if [ "$PRINT" == "True" ]; then
+    if [ "$verbose" == "True" ]; then
         echo $cmd1
         echo $cmd2
     fi
---
+++ install/scripts/sample_token_setup.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+/create_keys_on_disk.sh -v \
+    -n metadataaggregator \
+    -s /C=AT/ST=Wien/L=Wien/O=ABCfirma/OU=IT/CN=localhost
+
+/key_to_token.sh -d -i -n testtoken -s 12345678 -t 123456 \
+    -c metadataaggregator_crt.pem \
+    -k metadataaggregator_key_pkcs8.pem
