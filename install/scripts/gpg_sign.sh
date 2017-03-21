58f8b4a61d3b4c503e6214bded72803ba45a71a5
r2h2 <rainer@hoerbe.at>
2017-03-21 15:27:53 +0100

    + error handling

--- install/scripts/gpg_sign.sh
+++ install/scripts/gpg_sign.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+[[ -z "$GPG_SIGN_OPTIONS" ]] && echo "GPG_SIGN_OPTIONS not set" && exit 1
+[[ -z "$GPG_SIGNER" ]] && echo "GPG_SIGNER not set" && exit 1
 gpg2 --detach-sig $GPG_SIGN_OPTIONS -a --local-user $GPG_SIGNER --output $1.sig $1
 
