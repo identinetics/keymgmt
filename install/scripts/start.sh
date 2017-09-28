15c13081289cff2f0bea536931053a5da1a12d88
r2h2 <rainer@hoerbe.at>
2017-09-29 00:25:00 +0200

    fix call of startup_p11.sh in start.sh

--- install/scripts/start.sh
+++ install/scripts/start.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
+export PS4='+(${BASH_SOURCE}:${LINENO}): ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
 
 # main entrypoint of the docker container
 
-export PS4='+(${BASH_SOURCE}:${LINENO}): ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
 
 if [ $(id -u) -ne 0 ]; then
     echo 'Env variable PYKCS11LIB is set for root!'
     sudo='sudo'
 fi
 
-/startup_p11.sh
+/scripts/startup_p11.sh
 
 exec bash
