224584449cd986b859bfd2b0f98b6f20c297d2c9
r2h2 <rainer@hoerbe.at>
2017-11-16 08:27:50 +0100

    do sign testdoc when run by Jenkins

--- install/tests/test_gpg_card_sig.sh
+++ install/tests/test_gpg_card_sig.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
 source /scripts/gpg_setup.sh
-echo 'lore ipsum saget majstro humat kolunme zwazich loitra' > testdoc.txt
-/scripts/gpg_sign.sh testdoc.txt
+
+cd tests/testdata
+
+if [[ ! $JENKINS_HOME ]]; then
+    /scripts/gpg_sign.sh testdoc.txt  # requires user interaction
+fi
 
 gpg2 --verify testdoc.txt.sig testdoc.txt
